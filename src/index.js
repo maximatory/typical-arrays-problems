0536d75e61b980dc3ffc148becbf0aba6ee93439
Maksim Baranov <oooinfo@mail.ru>
2021-08-15 18:24:21 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (Object.keys(array).length == 0) {
+    return 0;
+  } else {
+    return Math.min.apply(Math, array);
+  }
+}
+
+exports.max = function max(array) {
+  return Math.max.apply(Math, array);
   return 0;
 }
 
-exports.max = function max (array) {
+exports.avg = function avg(array) {
+  let result = 0;
+  for (let i = 0; i < array.length; i++)
+    result += array[i];
+  return (result / array.length);
   return 0;
-}
+  };
 
-exports.avg = function avg (array) {
-  return 0;
-}
